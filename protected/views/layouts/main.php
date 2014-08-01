3ed1cd19ef0c88b4968e685907f2ea052f03e8f0
robludwig <robludwig@gmail.com>
2014-08-01 12:25:06 -0500

    edit the copyright footer thing

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -50,7 +50,7 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		Copyright &copy; <?php echo date('Y'); ?> by Robert Ludwig.<br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
